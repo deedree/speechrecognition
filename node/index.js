c4e34c5cde43e5f3b1b5f01bb51bb6860dce3b97
BWR <example@mail.com>
2020-02-28 18:06:47 +0100

    Tried to make an await function

--- node/index.js
+++ node/index.js
@@ -21,10 +21,70 @@ return files_;
 
 // console.log(getFiles('audioFiles'))
 
-
 let audioArray = getFiles('audioFiles')
 
-let singleAudioArray = ["audioFiles/Books/00 - Books - Caffaro gustav.wav", "./audioFiles/Homonyms/00 - Homonyms - dear deer gustav.wav", "./audioFiles/Homonyms/00 - Homonyms - dear deer vincent.wav"]; // 16000 Hz, Mono
+let singleAudioArray = [
+  // 'audioFiles/Books/00 - Books - Caffaro gustav.wav',
+  // 'audioFiles/Books/00 - Books - Caffaro ronald.wav',
+  // 'audioFiles/Books/00 - Books - Caffaro vincent.wav',
+  // 'audioFiles/Books/00 - Books - Palestine gustav.wav',
+  // 'audioFiles/Books/00 - Books - Palestine ronald.wav',
+  // 'audioFiles/Books/00 - Books - Palestine vincent.wav',
+  // 'audioFiles/Books/00 - Books - polling gustav.wav',
+  // 'audioFiles/Books/00 - Books - polling ronald.wav',
+  // 'audioFiles/Books/00 - Books - polling vincent.wav',
+  // 'audioFiles/Books/00 - Books - priority inversion gustav.wav',
+  // 'audioFiles/Books/00 - Books - priority inversion ronald.wav',
+  // 'audioFiles/Books/00 - Books - priority inversion vincent.wav',
+  // 'audioFiles/Books/00 - Books - semaphore ronald.wav',
+  // 'audioFiles/Books/00 - Books - semaphore vincent.wav',
+  // 'audioFiles/Books/00 - Books - short story gustav.wav',
+  // 'audioFiles/Books/00 - Books - short story ronald.wav',
+  // 'audioFiles/Books/00 - Books - short story vincent.wav',
+  // 'audioFiles/Books/00 - Books - submissive inferior gustav.wav',
+  // 'audioFiles/Books/00 - Books - submissive inferior ronald.wav',
+  // 'audioFiles/Books/00 - Books - submissive inferior vincent.wav',
+  // 'audioFiles/Homonyms/.DS_Store',
+  // 'audioFiles/Homonyms/00 - Homonyms - dear deer gustav.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - dear deer ronald.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - dear deer vincent.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - fort fight gustav.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - fort fight ronald.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - fort fight vincent.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - mail male gustav.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - mail male ronald.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - mail male vincent.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - meet meat gustav.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - meet meat ronald.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - meet meat vincent.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - scene seen gustav.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - scene seen ronald.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - scene seen vincent.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - weak week gustav.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - weak week ronald.wav',
+  // 'audioFiles/Homonyms/00 - Homonyms - weak week vincent.wav',
+  // 'audioFiles/Raw/00 - Raw - History todo cut gustav.wav',
+  // 'audioFiles/Raw/00 - Raw - technical book gustav.wav',
+  // 'audioFiles/Raw/00 - Raw - tongue twisters gustav.wav',
+  'audioFiles/TongueTwisters/00 - Tongue twisters - canner gustav.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - canner ronald.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - canner vincent.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - coffee gustav.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - coffee ronald.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - coffee vincent.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - fanny gustav.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - fanny ronald.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - fanny vincent.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - pest gustav.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - pest ronald.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - pest vincent.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - seashells gustav.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - seashells ronald.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - seashells vincent.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - wristwatches gustav.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - wristwatches ronald.wav',
+  // 'audioFiles/TongueTwisters/00 - Tongue twisters - wristwatches vincent.wav'
+]
 
 // console.log(audioArray)
 
@@ -61,32 +121,37 @@ const mapLoop = async _ => {
       
       // create the speech recognizer.
       var recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
-      
+
       // start the recognizer and wait for a result.
-      recognizer.recognizeOnceAsync(
-        async function (result) {
-          console.log(result.privText);
+      let thing = await recognizer.recognizeOnceAsync(
+          function lala(result) {
+          console.log(result);
       
           recognizer.close();
           recognizer = undefined;
+          console.log("IETS", typeof result)
+          return result
         },
         function (err) {
           console.trace("err - " + err);
       
           recognizer.close();
           recognizer = undefined;
-        });  
+        },
+       )  
+       await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+       console.log("TYPEOF", typeof iets)
     })
     
-    
-    
+  
+
   //   async recognizer => {
   //   const audioResult = await azureSpeech(recognizer)
   //   return audioResult
 
-  const audioResult = await Promise.all(promises)
-  console.log(audioResult)
-  console.log('End')
+  // const audioResult = await Promise.all(promises)
+  // console.log(audioResult)
+  // console.log('End')
 }
 
 mapLoop()
